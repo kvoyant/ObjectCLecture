58c8fa4a8f719c55aaa3198024d227b8e0ab292a
김영호 <kvoyant@gmail.com>
2020-07-05 10:57:21 +0900

    init

---
+++ SampleObjectC01/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SampleObjectC01
+//
+//  Created by 김영호 on 2020/07/05.
+//  Copyright © 2020 김영호. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
