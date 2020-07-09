3ab5bf75bafe27ba3420217bfc28c2551be3e437
kvoyant <kvoyant@gmail.com>
2020-07-10 00:37:16 +0900

    자식클래스 만들기

---
+++ SampleObjectC01/Square.h
@@ -0,0 +1,17 @@
+//
+//  Square.h
+//  SampleObjectC01
+//
+//  Created by APPLE on 2020/07/09.
+//  Copyright © 2020 김영호. All rights reserved.
+//
+
+#import "Rectangles.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Square : Rectangles
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SampleObjectC01/Square.m
@@ -0,0 +1,13 @@
+//
+//  Square.m
+//  SampleObjectC01
+//
+//  Created by APPLE on 2020/07/09.
+//  Copyright © 2020 김영호. All rights reserved.
+//
+
+#import "Square.h"
+
+@implementation Square
+
+@end
