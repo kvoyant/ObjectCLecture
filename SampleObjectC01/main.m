4be9743c532417bcefc128ec8f58b80bb216efc8
김영호 <kvoyant@gmail.com>
2020-07-25 13:28:48 +0900

    셀렉터 selector

--- SampleObjectC01/main.m
+++ SampleObjectC01/main.m
@@ -13,7 +13,35 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
+        /*
+         <셀렉터>
 
+         클래스에 선언된 메소드 구별하기
+         메소드 식별하려면? : 이름, 파라미터 개수, 레이블
+         메소드 선언
+            -(NSUInteger)length
+         셀렉터 => length
+         
+         ex) compare: , compare:options: , compare:options:range:
+         
+         <컴파일드 셀렉터>
+         
+         코드로 셀렉터 사용
+         형태 : @selector(xxx)
+         SEL 타입 : SEL s=@selector(length)
+         */
+        
+        NSString *str = @"hello world";
+        SEL s = @selector(uppercaseString);
+        NSString *ret = [str performSelector:s];
+        NSLog(@"Result : %@",ret);
+        
+        SEL s2 = @selector(abcd);
+        
+        if([str respondsToSelector:s2])
+            [str performSelector:s2];//unrecognized selector error occur !!
+        else
+            NSLog(@"abcd 셀렉터 호출 불가!");
         
     }
     return 0;
